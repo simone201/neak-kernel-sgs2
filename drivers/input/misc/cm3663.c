b96f52e5913004ec519854b153888efb9a6e27ea
craig <hot2009wheels@hotmail.com>
2011-08-30 04:03:59 +0100

    cm3663: set initial state of proximity sensor to "far" (codeworkx)

--- drivers/input/misc/cm3663.c
+++ drivers/input/misc/cm3663.c
@@ -798,6 +798,10 @@ static int cm3663_i2c_probe(struct i2c_client *client,
 		goto err_light_device_create_file;
 	}
 	dev_set_drvdata(cm3663->switch_cmd_dev, cm3663);
+	
+	/* 07-28-2011 codeworkx: set initial proximity value as 1 */
+	input_report_abs(cm3663->proximity_input_dev, ABS_DISTANCE, 1);
+	input_sync(cm3663->proximity_input_dev);
 
 	goto done;
 
