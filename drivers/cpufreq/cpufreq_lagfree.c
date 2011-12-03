7fc5d673992b35db5a17504c7ca1d51bad4d9635
Simone Renzo <simone201@gmail.com>
2011-12-03 15:57:09 +0100

    Again tweaking lagfree governor

--- drivers/cpufreq/cpufreq_lagfree.c
+++ drivers/cpufreq/cpufreq_lagfree.c
@@ -35,8 +35,8 @@
  * It helps to keep variable names smaller, simpler
  */
 
-#define DEF_FREQUENCY_UP_THRESHOLD				(60)
-#define DEF_FREQUENCY_DOWN_THRESHOLD				(15)
+#define DEF_FREQUENCY_UP_THRESHOLD				(65)
+#define DEF_FREQUENCY_DOWN_THRESHOLD				(20)
 #define FREQ_STEP_DOWN 						(100000)
 #define FREQ_SLEEP_MAX 						(500000)
 #define FREQ_AWAKE_MIN 						(100000)
