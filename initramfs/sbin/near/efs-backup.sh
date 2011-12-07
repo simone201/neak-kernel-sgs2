1eaf110d2c6a916b6802ce257b6980bec4e0d392
Simone Renzo <simone201@gmail.com>
2011-12-07 16:50:48 +0100

    Fixed efs backup via cwm

--- initramfs/sbin/near/efs-backup.sh
+++ initramfs/sbin/near/efs-backup.sh
@@ -1,7 +1,5 @@
 #!/sbin/busybox sh
 
-/sbin/busybox mount -o rw /dev/block/mmcblk0p11 /mnt/sdcard
-
 if ! [ -d /sdcard/near ]; then
 	mkdir  /sdcard/near
 else
@@ -11,10 +9,8 @@ fi;
 if [ ! -f /sdcard/near/efsbackup.tar.gz ];
 then
   /sbin/busybox tar zcvf /sdcard/near/efsbackup.tar.gz /efs
-  /sbin/busybox cat /dev/block/mmcblk0p1 > /sdcard/near/efsdev-mmcblk0p1.img
+  /sbin/busybox dd if=/dev/block/mmcblk0p1 of=/sdcard/near/efsdev-mmcblk0p1.img bs=4096
   /sbin/busybox gzip /sdcard/near/efsdev-mmcblk0p1.img
 else
 	echo "efs backup already exists"
 fi;
-
-/sbin/busybox umount /mnt/sdcard
