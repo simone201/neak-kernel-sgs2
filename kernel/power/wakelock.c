551d6d65b30ad14ccdd744af2a41473639bd370b
Simone Renzo <simone201@gmail.com>
2011-12-09 15:49:09 +0100

    Fixed missing function

--- kernel/power/wakelock.c
+++ kernel/power/wakelock.c
@@ -45,6 +45,7 @@ static LIST_HEAD(inactive_locks);
 static struct list_head active_wake_locks[WAKE_LOCK_TYPE_COUNT];
 static int current_event_num;
 struct workqueue_struct *suspend_work_queue;
+struct wake_lock main_wake_lock;
 suspend_state_t requested_suspend_state = PM_SUSPEND_MEM;
 static struct wake_lock unknown_wakeup;
 static struct wake_lock suspend_backoff_lock;
