0fb5b7b1f13575e944a9635d4d26f500db750591
Simone Renzo <simone201@gmail.com>
2011-12-16 19:33:29 +0100

    [PATCH] kernel:smp: Match on_each_cpu behaviour on UP to SMP
    The comment in the SMP implementation of on_each_cpu says:
    "May be used during early boot while
    early_boot_irqs_disabled is set. Use local_irq_save/restore() instead
    of local_irq_disable/enable()."
    
    but the existing UP macro version uses local_irq_disable/enable().
    
    Do the safe thing - make the UP version match the SMP one.
    
    Signed-off-by: Gilad Ben-Yossef <gilad@benyossef.com>
    CC: Jiri Kosina <trivial@kernel.org>
    CC: Randy Dunlap <rdunlap@xenotime.net>
    
    Signed-off-by: Sar Castillo <sar.castillo@gmail.com>

--- include/linux/smp.h
+++ include/linux/smp.h
@@ -129,11 +129,12 @@ static inline int up_smp_call_function(void (*func)(void *), void *info)
 #define smp_call_function(func, info, wait) \
 			(up_smp_call_function(func, info))
 #define on_each_cpu(func,info,wait)		\
-	({					\
-		local_irq_disable();		\
-		func(info);			\
-		local_irq_enable();		\
-		0;				\
+	({									\
+		unsigned long __flags;			\
+		local_irq_save(__flags);		\
+		func(info);						\
+		local_irq_restore(__flags);		\
+		0;								\
 	})
 static inline void smp_send_reschedule(int cpu) { }
 #define num_booting_cpus()			1
