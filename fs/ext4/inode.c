d31ef113c9adf3e1c434614f15f52fec22e93b20
Simone Renzo <simone201@gmail.com>
2011-12-21 22:38:10 +0100

    ext4: fix ext4_end_io_dio() racing against fsync()
    We need to make sure iocb->private is cleared *before* we put the
    io_end structure on i_completed_io_list.  Otherwise fsync() could
    potentially run on another CPU and free the iocb structure out from
    under us.
    
    Reported-by: Kent Overstreet <koverstreet@google.com>
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>
    Cc: stable@kernel.org

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -3810,11 +3810,12 @@ static void ext4_end_io_dio(struct kiocb *iocb, loff_t offset,
 		  "for inode %lu, iocb 0x%p, offset %llu, size %llu\n",
  		  iocb->private, io_end->inode->i_ino, iocb, offset,
 		  size);
+		  
+	iocb->private = NULL;
 
 	/* if not aio dio with unwritten extents, just free io and return */
 	if (io_end->flag != EXT4_IO_UNWRITTEN){
 		ext4_free_io_end(io_end);
-		iocb->private = NULL;
 out:
 		if (is_async)
 			aio_complete(iocb, ret, 0);
