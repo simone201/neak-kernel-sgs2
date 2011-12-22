bffcea3b71957a723374110d45b2d22d830f56a8
Simone Renzo <simone201@gmail.com>
2011-12-22 22:35:39 +0100

    N.E.A.K. v1.1x

--- initramfs/sbin/post-init.sh
+++ initramfs/sbin/post-init.sh
@@ -1,7 +1,7 @@
 #!/sbin/busybox sh
 # thanks to hardcore and nexxx
 # some parameters are taken from http://forum.xda-developers.com/showthread.php?t=1292743 (highly recommended to read)
-# modded by simone201 for Dawn Kernel
+# modded by simone201 for NEAK Kernel
 
 MMC=`ls -d /sys/block/mmc*`;
 
