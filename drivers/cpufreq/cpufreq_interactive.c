694493f7d77bb0f47eb127cd0668ac3280e357c9
Simone Renzo <simone201@gmail.com>
2012-01-04 22:56:59 +0100

    Reworked interactive gov based on siyah one

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -180,12 +180,6 @@ static void cpufreq_interactive_timer(unsigned long data)
 	u64 now_idle;
 	unsigned int new_freq;
 	unsigned int index;
-	unsigned long flags;
-
-	smp_rmb();
-
-	if (!pcpu->governor_enabled)
-		goto exit;
 
 	/*
 	 * Once pcpu->timer_run_time is updated to >= pcpu->idle_exit_time,
@@ -286,18 +280,18 @@ static void cpufreq_interactive_timer(unsigned long data)
 
 	if (new_freq < pcpu->target_freq) {
 		pcpu->target_freq = new_freq;
-		spin_lock_irqsave(&down_cpumask_lock, flags);
+		spin_lock(&down_cpumask_lock);
 		cpumask_set_cpu(data, &down_cpumask);
-		spin_unlock_irqrestore(&down_cpumask_lock, flags);
+		spin_unlock(&down_cpumask_lock);
 		queue_work(down_wq, &freq_scale_down_work);
 	} else {
 		pcpu->target_freq = new_freq;
 #if DEBUG
 		up_request_time = ktime_to_us(ktime_get());
 #endif
-		spin_lock_irqsave(&up_cpumask_lock, flags);
+		spin_lock(&up_cpumask_lock);
 		cpumask_set_cpu(data, &up_cpumask);
-		spin_unlock_irqrestore(&up_cpumask_lock, flags);
+		spin_unlock(&up_cpumask_lock);
 		wake_up_process(up_task);
 	}
 
@@ -408,8 +402,7 @@ static void cpufreq_interactive_idle(void)
 	 * run.)
 	 */
 	if (timer_pending(&pcpu->cpu_timer) == 0 &&
-	    pcpu->timer_run_time >= pcpu->idle_exit_time &&
-	    pcpu->governor_enabled) {
+	    pcpu->timer_run_time >= pcpu->idle_exit_time) {
 		pcpu->time_in_idle =
 			get_cpu_idle_time_us(smp_processor_id(),
 					     &pcpu->idle_exit_time);
@@ -430,7 +423,6 @@ static int cpufreq_interactive_up_task(void *data)
 {
 	unsigned int cpu;
 	cpumask_t tmp_mask;
-	unsigned long flags;
 	struct cpufreq_interactive_cpuinfo *pcpu;
 
 #if DEBUG
@@ -441,16 +433,16 @@ static int cpufreq_interactive_up_task(void *data)
 
 	while (1) {
 		set_current_state(TASK_INTERRUPTIBLE);
-		spin_lock_irqsave(&up_cpumask_lock, flags);
+		spin_lock(&up_cpumask_lock);
 
 		if (cpumask_empty(&up_cpumask)) {
-			spin_unlock_irqrestore(&up_cpumask_lock, flags);
+			spin_unlock(&up_cpumask_lock);
 			schedule();
 
 			if (kthread_should_stop())
 				break;
 
-			spin_lock_irqsave(&up_cpumask_lock, flags);
+			spin_lock(&up_cpumask_lock);
 		}
 
 		set_current_state(TASK_RUNNING);
@@ -469,7 +461,7 @@ static int cpufreq_interactive_up_task(void *data)
 
 		tmp_mask = up_cpumask;
 		cpumask_clear(&up_cpumask);
-		spin_unlock_irqrestore(&up_cpumask_lock, flags);
+		spin_unlock(&up_cpumask_lock);
 
 		for_each_cpu(cpu, &tmp_mask) {
 			pcpu = &per_cpu(cpuinfo, cpu);
@@ -478,11 +470,6 @@ static int cpufreq_interactive_up_task(void *data)
 				dbgpr("up %d: tgt=%d nothing else running\n", cpu,
 				      pcpu->target_freq);
 			}
-
-			smp_rmb();
-
-			if (!pcpu->governor_enabled)
-				continue;
 
 			__cpufreq_driver_target(pcpu->policy,
 						pcpu->target_freq,
@@ -501,22 +488,15 @@ static void cpufreq_interactive_freq_down(struct work_struct *work)
 {
 	unsigned int cpu;
 	cpumask_t tmp_mask;
-	unsigned long flags;
 	struct cpufreq_interactive_cpuinfo *pcpu;
 
-	spin_lock_irqsave(&down_cpumask_lock, flags);
+	spin_lock(&down_cpumask_lock);
 	tmp_mask = down_cpumask;
 	cpumask_clear(&down_cpumask);
-	spin_unlock_irqrestore(&down_cpumask_lock, flags);
+	spin_unlock(&down_cpumask_lock);
 
 	for_each_cpu(cpu, &tmp_mask) {
 		pcpu = &per_cpu(cpuinfo, cpu);
-
-		smp_rmb();
-
-		if (!pcpu->governor_enabled)
-			continue;
-
 		__cpufreq_driver_target(pcpu->policy,
 					pcpu->target_freq,
 					CPUFREQ_RELATION_H);
@@ -587,7 +567,6 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *new_policy,
 			get_cpu_idle_time_us(new_policy->cpu,
 					     &pcpu->freq_change_time);
 		pcpu->governor_enabled = 1;
-		smp_wmb();
 		/*
 		 * Do not register the idle hook and create sysfs
 		 * entries if we have already done so.
@@ -606,16 +585,6 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *new_policy,
 
 	case CPUFREQ_GOV_STOP:
 		pcpu->governor_enabled = 0;
-		smp_wmb();
-		del_timer_sync(&pcpu->cpu_timer);
-		flush_work(&freq_scale_down_work);
-		/*
-		 * Reset idle exit time since we may cancel the timer
-		 * before it can run after the last idle exit time,
-		 * to avoid tripping the check in idle exit for a timer
-		 * that is trying to run.
-		 */
-		pcpu->idle_exit_time = 0;
 
 		if (atomic_dec_return(&active_count) > 0)
 			return 0;
@@ -624,6 +593,7 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *new_policy,
 				&interactive_attr_group);
 
 		pm_idle = pm_idle_old;
+		del_timer(&pcpu->cpu_timer);
 		break;
 
 	case CPUFREQ_GOV_LIMITS:
@@ -673,14 +643,13 @@ static int __init cpufreq_interactive_init(void)
 	INIT_WORK(&freq_scale_down_work,
 		  cpufreq_interactive_freq_down);
 
-	spin_lock_init(&up_cpumask_lock);
-	spin_lock_init(&down_cpumask_lock);
-
 #if DEBUG
 	spin_lock_init(&dbgpr_lock);
 	dbg_proc = create_proc_entry("igov", S_IWUSR | S_IRUGO, NULL);
 	dbg_proc->read_proc = dbg_proc_read;
 #endif
+	spin_lock_init(&down_cpumask_lock);
+	spin_lock_init(&up_cpumask_lock);
 
 	return cpufreq_register_governor(&cpufreq_gov_interactive);
 
@@ -709,4 +678,3 @@ MODULE_AUTHOR("Mike Chan <mike@android.com>");
 MODULE_DESCRIPTION("'cpufreq_interactive' - A cpufreq governor for "
 	"Latency sensitive workloads");
 MODULE_LICENSE("GPL");
-
