4081dd2ca5d8a0cde525b399e8412b2175317d9b
Simone Renzo <simone201@gmail.com>
2012-01-08 13:49:00 +0100

    Changed init.d support way of executing scripts

---
+++ initramfs/sbin/near/init-support.sh
@@ -0,0 +1,20 @@
+#!/sbin/busybox sh
+# Init.d support for N.E.A.K. Kernel
+# by Simone201
+
+if [ -d /system/etc/init.d ]; then
+  /sbin/busybox run-parts /system/etc/init.d
+fi;
+
+if [ -d /data/init.d ]; then
+  /sbin/busybox run-parts /data/init.d
+fi;
+
+# fix for samsung roms - setting scaling_max_freq - thx to gokhanmoral
+freq=`cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq`
+if [ "$freq" != "1200" ];then
+  (
+   sleep 25;
+   echo $freq > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
+  ) &
+fi
--- initramfs/sbin/post-init.sh
+++ initramfs/sbin/post-init.sh
@@ -153,49 +153,9 @@ echo $(date) PRE-INIT DONE of post-init.sh
 sleep 10
 
 # init.d support
-echo $(date) USER EARLY INIT START from /system/etc/init.d
-if cd /system/etc/init.d >/dev/null 2>&1 ; then
-    for file in E* ; do
-        if ! cat "$file" >/dev/null 2>&1 ; then continue ; fi
-        echo "START '$file'"
-        /system/bin/sh "$file"
-        echo "EXIT '$file' ($?)"
-    done
-fi
-echo $(date) USER EARLY INIT DONE from /system/etc/init.d
-
-echo $(date) USER EARLY INIT START from /data/init.d
-if cd /data/init.d >/dev/null 2>&1 ; then
-    for file in E* ; do
-        if ! cat "$file" >/dev/null 2>&1 ; then continue ; fi
-        echo "START '$file'"
-        /system/bin/sh "$file"
-        echo "EXIT '$file' ($?)"
-    done
-fi
-echo $(date) USER EARLY INIT DONE from /data/init.d
-
-echo $(date) USER INIT START from /system/etc/init.d
-if cd /system/etc/init.d >/dev/null 2>&1 ; then
-    for file in S* ; do
-        if ! ls "$file" >/dev/null 2>&1 ; then continue ; fi
-        echo "START '$file'"
-        /system/bin/sh "$file"
-        echo "EXIT '$file' ($?)"
-    done
-fi
-echo $(date) USER INIT DONE from /system/etc/init.d
-
-echo $(date) USER INIT START from /data/init.d
-if cd /data/init.d >/dev/null 2>&1 ; then
-    for file in S* ; do
-        if ! ls "$file" >/dev/null 2>&1 ; then continue ; fi
-        echo "START '$file'"
-        /system/bin/sh "$file"
-        echo "EXIT '$file' ($?)"
-    done
-fi
-echo $(date) USER INIT DONE from /data/init.d
+echo $(date) INIT.D SUPPORT START
+/sbin/busybox sh /sbin/near/init-support.sh
+echo $(date) INIT.D SUPPORT DONE
 
 /sbin/busybox sh /sbin/near/bln.sh
 
